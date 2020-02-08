3d3bf131ff1c0a8b8430bda1ab447aabc4390d6f
Alex  Griffiths <alex.griffiths18@imperial.ac.uk>
2020-02-08 13:26:31 +0000

    Added base test files from online tutorial

---
+++ src/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('test');
+  checkPageButton.addEventListener('click', function() {
+
+  }, false);
+}, false);
